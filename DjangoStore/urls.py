7d323d1c2cd8f8fdb26cd758f5999ae41ec4c8ea
Eden Iyanda <edeniyanda@gmail.com>
2024-07-10 15:24:01 +0100

    Created Contact html file

--- DjangoStore/urls.py
+++ DjangoStore/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
-from core.views import index
+from core.views import (index, contact)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name="home_page"),
+    path('contact/', contact, name="contact_page"),
 ]
