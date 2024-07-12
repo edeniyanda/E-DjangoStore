625b6497e4f4cdb716f924e66b64886b49460de1
Eden Iyanda <edeniyanda@gmail.com>
2024-07-12 16:28:14 +0100

    feat: Spinned up html for the dashboard page

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from item.models import Item
 
 
 @login_required
 def index(request):
-    return render(request, 'dashboard/index.html', {})
+    items_by_user = Item.objects.filter(created_by=request.user)
+    return render(request, 'dashboard/index.html', {
+        "items" : items
+    })
