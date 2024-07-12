f360dc704659ec8ae0d02c21e7b84078d1bddd4b
Eden Iyanda <edeniyanda@gmail.com>
2024-07-12 13:46:20 +0100

    Designed the LOgin Form

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
+from django.contrib.auth import views as auth_view
 from .views import index, contact, signup
-
+from .forms import LoginForm
 
 app_name = "core"
 
@@ -8,4 +9,5 @@ urlpatterns = [
     path("", index, name="home_page"),
     path("contact/", contact, name="contact_page"),
     path("signup/", signup, name="signup_page"),
+    path("login/", auth_view.LoginView.as_view(template_name="core/login.html", authentication_form=LoginForm), name="login_page"),
 ]
