f9ebfa0aab8f844ebbb5e4a46fe51aeddb419a6b
Eden Iyanda <edeniyanda@gmail.com>
2024-07-12 13:53:22 +0100

    Update: Added login and logout redirect URLs to settings

--- DjangoStore/settings.py
+++ DjangoStore/settings.py
@@ -126,6 +126,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn/')
 MEDIA_URL = 'media/'
 MEDIA_ROOT =  BASE_DIR / 'media/'
 
+
+LOGIN_REDIRECT_URL = "/"
+LOGIN_URL = "/login/"
+LOGOUT_REDIRECT_URL = "/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
