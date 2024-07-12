9c1b0b1d2c376c9a1908a6076c060d8ad9e195a9
Eden Iyanda <edeniyanda@gmail.com>
2024-07-12 12:54:23 +0100

    Revert "feat: Style sign-up form fields with custom attributes in forms.py"
    This reverts commit 0831c9ecf3b3abebf047cb5882a4ad653c0afd51.

--- core/forms.py
+++ core/forms.py
@@ -6,20 +6,3 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
-
-    username = forms.CharField(widget=forms.TextInput(attrs={
-        "placeholder": "Enter a Username",
-        "class": "form-control"
-    }))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={
-        "placeholder": "Enter your Email",
-        "class": "form-control"
-    }))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
-        "placeholder": "Password",
-        "class": "form-control"
-    }))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
-        "placeholder": "Confirm Password",
-        "class": "form-control"
-    }))
