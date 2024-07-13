29c3b68960eb6037a71263f91b7175a686d39a2f
Eden Iyanda <edeniyanda@gmail.com>
2024-07-13 13:06:23 +0100

    Update: Allowed users to search based on category

--- item/views.py
+++ item/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
-from .models import Item
+from .models import Item, Category
 from .forms import NewItemForm, EditItemForm
 
 def detail(request, pk):
@@ -14,14 +14,21 @@ def detail(request, pk):
 
 def itemsall(request):
     query = request.GET.get("query", "")
+    category_id = request.GET.get("category", 0)
     items = Item.objects.filter(is_sold=False)
+    categories = Category.objects.all()
+
+    if category_id:
+        items = items.filter(category = category_id)
 
     if query:
-        items = items.filter(Q(name__icontains=query) | Q(description_icontains=query))
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
 
     return render(request, "item/itemsall.html", {
         "items" : items, 
-        "query" : query
+        "query" : query,
+        "categories": categories,
+        "category_id" : category_id
     })
 
 @login_required
