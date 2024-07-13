d73980d3a0d8e92ad3bd5b7b277a9201d65744dd
Eden Iyanda <edeniyanda@gmail.com>
2024-07-13 11:56:29 +0100

    Designed the sidebar for user to search item

--- item/urls.py
+++ item/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "item"
 
 urlpatterns = [
+    path("", views.itemsall, name="all_item"),
     path("newitem/", views.newitem, name="new_item"),
     path("<int:pk>/", views.detail, name="detail"),
     path("<int:pk>/delete", views.delete_item, name="delete_item"),
